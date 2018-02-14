4f61800f9415bb965febe4a6e29af30cf0eb27db
Unknown <gersoncalienes@gmail.com>
2018-02-14 13:36:31 -0500

    Added jquery

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<button id="js-get-info">get-info</button>
+				<textarea name="" id="js-show-info" cols="30" rows="10" />
+			</div>
+		);
+	}
 }
 
 export default App;
