1958fc585f90142f291f994665780c977bd77ece
Unknown <gersoncalienes@gmail.com>
2018-02-14 14:20:36 -0500

    Accessed the propublica congress API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 import $ from 'jquery';
 
+// $('#js-get-info').on('click', () => {
+// 	$.ajax({
+// 		type: 'GET',
+// 		url: 'https://api.propublica.org/congress/v1/115/explanations.json',
+// 		headers: {
+// X-API-Key:ErXQPoib5ox5eDm1V0gJPguS25miL2ehc0boLHew
+// 			// 'X-API-Key': 'ErXQPoib5ox5eDm1V0gJPguS25miL2ehc0boLHew'
+// 		},
+// 		success: function(response) {
+// 			console.log(response);
+// 		},
+// 		error: function(e) {
+// 			console.log(e);
+// 		}
+// 	});
+// });
+
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			posts: []
+		};
+	}
+
+	componentDidMount() {
+		axios
+			.get(`https://api.propublica.org/congress/v1/115/explanations.json`, {
+				headers: { 'X-API-Key': 'ErXQPoib5ox5eDm1V0gJPguS25miL2ehc0boLHew' }
+			})
+			.then(res => {
+				console.log(res);
+			});
+	}
 	render() {
 		return (
 			<div className="App">
