4f1279d447a95401f215bf3b7174b374fdd1dbeb
Windows <gersoncalienes@gmail.com>
2018-02-20 11:03:25 -0500

    Modified state to include senate and house members

--- src/App.js
+++ src/App.js
@@ -21,31 +21,49 @@ import $ from 'jquery';
 // });
 
 class App extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			posts: []
-		};
-	}
+    this.state = {
+      congressNumber: 115,
+      chamber: 'senate',
+      senateMembers: [],
+      houseMembers: []
+    };
+  }
 
-	componentDidMount() {
-		axios
-			.get(`https://api.propublica.org/congress/v1/115/explanations.json`, {
-				headers: { 'X-API-Key': 'ErXQPoib5ox5eDm1V0gJPguS25miL2ehc0boLHew' }
-			})
-			.then(res => {
-				console.log(res);
-			});
-	}
-	render() {
-		return (
-			<div className="App">
-				<button id="js-get-info">get-info</button>
-				<textarea name="" id="js-show-info" cols="30" rows="10" />
-			</div>
-		);
-	}
+  async getData() {
+    // console.log(this.state);
+    const response = await axios.get(
+      // `https://api.propublica.org/congress/v1/115/explanations.json`, congress voting explanations
+      `https://api.propublica.org/congress/v1/${this.state.congressNumber}/${
+        this.state.chamber
+      }/members.json`, //https://projects.propublica.org/api-docs/congress-api/members/
+      {
+        headers: { 'X-API-Key': 'ErXQPoib5ox5eDm1V0gJPguS25miL2ehc0boLHew' }
+      }
+    );
+    const data = response.data.results;
+    // console.log(data[0].members);
+    if (this.state.chamber === 'senate') {
+      this.setState({ senateMembers: data[0].members });
+    } else if (this.state.chamber === 'house') {
+      this.setState({ houseMembers: data[0].members });
+    }
+    console.log(this.state.houseMembers);
+    console.log(this.state.senateMembers);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button id="js-get-info" onClick={() => this.getData()}>
+          get-info
+        </button>
+        <textarea name="" id="js-show-info" cols="30" rows="10" />
+      </div>
+    );
+  }
 }
 
 export default App;
