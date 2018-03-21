94289778b183cede5f91de8abbc159d237984711
Windows <gersoncalienes@gmail.com>
2018-03-21 15:09:15 -0400

    Delete materialUI

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import injectTapEventPlugin from 'react-tap-event-plugin'; // add
-
 ReactDOM.render(<App />, document.getElementById('root'));
-injectTapEventPlugin();
 
 registerServiceWorker();
