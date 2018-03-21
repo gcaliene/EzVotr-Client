c3aed0f68a821ca81f6ca949cfd3a94f7931e2b1
Unknown <gersoncalienes@gmail.com>
2018-03-21 15:04:46 -0400

    Deleted some comment and reverted back to original

--- src/App.js
+++ src/App.js
@@ -4,36 +4,19 @@ import $ from 'jquery';
 
 import PurpleOptions from './components/purple_options';
 
-// $('#js-get-info').on('click', () => {
-// 	$.ajax({
-// 		type: 'GET',
-// 		url: 'https://api.propublica.org/congress/v1/115/explanations.json',
-// 		headers: {
-// X-API-Key:ErXQPoib5ox5eDm1V0gJPguS25miL2ehc0boLHew
-// 			// 'X-API-Key': 'ErXQPoib5ox5eDm1V0gJPguS25miL2ehc0boLHew'
-// 		},
-// 		success: function(response) {
-// 			console.log(response);
-// 		},
-// 		error: function(e) {
-// 			console.log(e);
-// 		}
-// 	});
-// });
-
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+	// constructor(props) {
+	//   super(props);
+	// }
 
-  render() {
-    return (
-      <div className="App">
-        <PurpleOptions />
-        <div className="left dem-blue" />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<PurpleOptions />
+				<div className="left dem-blue" />
+			</div>
+		);
+	}
 }
 
 export default App;
